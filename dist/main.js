03f8c469f9a041e02d98ce65fb3300729c78d102
Rak-ib <19rakib120@gmail.com>
2025-01-06 19:03:21 +0600

    vercel.json upadted

--- dist/main.js
+++ dist/main.js
@@ -5,6 +5,7 @@ const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    console.log('FRONTEND_URL:', process.env.FRONTEND_URL);
     app.enableCors({
         origin: process.env.FRONTEND_URL,
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('FRONTEND_URL:', process.env.FRONTEND_URL);
   app.enableCors({
     origin: process.env.FRONTEND_URL, // Frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
