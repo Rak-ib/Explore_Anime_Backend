7f42c3302d082933b0f339c89c755f4dd7eabd2e
Rak-ib <19rakib120@gmail.com>
2025-02-11 20:51:53 +0600

    checking

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //console.log('FRONTEND_URL:', process.env.FRONTEND_URL);
   app.enableCors({
-    origin: process.env.FRONTEND_URL, // Frontend URL
+    origin: 'https://exploreanime.vercel.app', // Frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials:true,
   });
